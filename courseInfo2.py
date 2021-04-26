2b0e151fa042c2343f2a916cf4df043b37fd874d
galigaribaldi <galigaribaldi@live.com>
2021-04-26 02:39:48 -0500

    V1.0

--- courseInfo2.py
+++ courseInfo2.py
@@ -5,113 +5,136 @@ import os.path
 import pickle
 ###Modulo propio
 import models as model
-
+##Pandas
+import pandas as pd
+##Retorna Dataframe: id Curso, nombre Curso, Descripcion Curso
 def get_all_courses():
     service = model.returns_service()
     results = service.courses().list(pageSize=10).execute()
-    courses = []
+    #courses = []
+    cont = 0
+    df = pd.DataFrame(columns=['ID_Curso', 'Nombre_Curso', 'Descripcion'], index=range(len(results['courses'])))
     for i in results['courses']:
         l = []
-        #print(i['id']);print(i['name']);print(i['descriptionHeading'])
         l.append(i['id'])
         l.append(i['name'])
         l.append(i['descriptionHeading'])
-        courses.append(l)
-    return courses
+        df.iloc[cont] = l
+        cont = cont+1
+    return df
+
+#c = get_all_courses()
+#print(c)
+###Retorna Dataframe: ID, nombre, email
 def get_users_by_idCourse(Courses_id):
     service = model.returns_service()
     results = service.courses().students().list(courseId=Courses_id)
     studentCourses =results.execute()
-    studentInfo=[]
+    df = pd.DataFrame(columns=['StudentID', 'Student_Name', 'EmailStudent'], index=range(len(studentCourses['students'])))
+    cont = 0
     for i in studentCourses['students']:
         l = []
         #print(i['userId']);print(i['profile']['name']['fullName']);print(i['profile']['emailAddress'])
-        l.append(i['userId'])
-        l.append(i['profile']['name']['fullName'])
-        l.append(i['profile']['emailAddress'])
-        studentInfo.append(l)
-    return studentInfo
-    
-def viewTeachers(Courses_id):
-    service = model.returns_service()
-    results = service.courses().teachers().list(courseId=Courses_id)
-    print(results, type(results))
+        try:
+            l.append(i['userId'])
+        except KeyError:
+            l.append("NULL")
+        try:
+            l.append(i['profile']['name']['fullName'])
+        except KeyError:
+            l.append("NULL")
+        try:
+            l.append(i['profile']['emailAddress'])            
+        except KeyError:
+            l.append("NULL")
+        df.iloc[cont] = l
+        cont =cont+1
+    return df
+#c = get_users_by_idCourse(126383120623)
+#print(c)
 
-def get_course_by_id(course_id):
-    """ Retrieves a classroom course by its id. """
-    service = model.returns_service()
-    try:
-        course = service.courses().get(id=course_id).execute()
-        #print('Course "{%s}" found.' % course.get('name'))
-    except errors.HttpError as error:
-        #print('Course with ID "{%s}" not found.' % course_id)
-    # [END classroom_get_course]
-        return error
-    return course
-
-def courses_list_activities(course_id):
+##Retorna Dataframe: Titulo, Descripcion, FechaCreacion, Maxpuntos,Fechaentrega
+def courses_list_activities(course_id, mesFiltro, anioFiltro):
     service = model.returns_service()
     results = service.courses().courseWork().list(courseId=course_id).execute()
     list_activities = []
+    df = pd.DataFrame(columns=['CourseWorkID', 'Titulo', 'Descripcion','FechaCreacion','PuntosMaximos','FechaEntrega'], index=range(len(results['courseWork'])))
+    cont = 0
     for i in results['courseWork']:
         l = []
-        #print(i['id'])
-        l.append(i['id'])
-        
+        #print(i)
+        ##print(i.keys())
+        #input()
         try:
-            #print(i['title'])
-            l.append(i['title'])
-        except:
-            l.append("Sin Titulo")
-        
-        try:
-            #print(i['description'])
-            l.append(i['description'])
-        except:
-            #print("Sin decripcion")
-            l.append("Sin decripcion")
-        
-        try:
-            #print(i['creationTime'])
-            l.append(i['creationTime'])
-        except:
-            l.append("Sin hora")
-        
-        try:
-            #print(i['maxPoints'])
-            l.append(i['maxPoints'])
+            if i['dueDate']['month'] >mesFiltro and i['dueDate']['year'] == anioFiltro:
+                l.append(i['id'])
+                try:
+                    l.append(i['title'])
+                except:
+                    l.append("Sin Titulo")
+                
+                try:
+                    l.append(i['description'])
+                except:
+                    l.append("Sin decripcion")
+                try:
+                    l.append(i['creationTime'])
+                except:
+                    l.append("Sin hora")
+                try:
+                    l.append(i['maxPoints'])
+                except KeyError:
+                    l.append("Sin puntaje")
+                try:
+                    d = str(i['dueDate']['day']) + '/' +str(i['dueDate']['month']) +'/' +str(i['dueDate']['year'])
+                    l.append(d)
+                except KeyError:
+                    l.append("Fecha")
+                df.iloc[cont] = l
+                cont = cont+1
         except KeyError:
-            #print("Sin puntaje")
-            l.append("Sin puntaje")
-        list_activities.append(l)
-    return list_activities
+            pass
+    df = df.dropna()
+    return df
+#c = courses_list_activities(126383120623, 1,2021)
+#print(c)
 
-def sumbiss_student_courseWork(course_id, course_work_id, student_id):
+def sumbiss_student_courseWork(course_id, course_work_id):
     service = model.returns_service()
     results = service.courses().courseWork().studentSubmissions().list(courseId=course_id, courseWorkId=course_work_id).execute()
-    #print(results)
+    df = pd.DataFrame(columns=['CourseID', 'CourseWorkID', 'StudentID','FechaCreacion','State','ArchivoEntrega','Calificacion'], index=range(len(results['studentSubmissions'])))
+    cont = 0
     for i in results['studentSubmissions']:
-        print("Fecha de Dejado", i['creationTime'])
-        print(i['state'])
+        l = []
+        #print(i)
+        #input()
         try:
-            print(i['assignedGrade'])
-        except KeyError:
-            print("sin calificacion")
+            l.append(i["courseId"]);l.append(i["courseWorkId"])
+        except:
+            l.append("NULL")
         try:
-            print(i['updateTime'])
-        except KeyError:
-            print("sin Entregar")
-        print(i['userId'])
-        print("\n")
-        print(i)
-        print("\n")
+            l.append(i["userId"]);l.append(i["creationTime"])
+        except:
+            l.append("NULL")
+        try:
+            l.append(i["state"])
+        except:
+            l.append("NULL")
+        try:
+            l.append(i["assignmentSubmission"]['attachments'][0]['driveFile']['title'])
+        except:
+            l.append("NULL")
+        try:
+            l.append(int(i["draftGrade"])/10)
+        except:
+            l.append(0)
+        df.iloc[cont] = l
+        cont = cont+1
+    return df
 #c = get_course_by_id(275450839438)
 #c = get_users_by_idCourse(275450839438)
 #c = get_all_courses()
-CourseworkId=326779603805
+#print(c)
+CourseworkId=276311125107
 StudentId=113580004965193682564
-CourseId = 275450839438
-#c = sumbiss_student_courseWork(CourseId,CourseworkId,StudentId)
-
-
-courses_list_activities(CourseId)
+CourseId = 126382145220
---
+++ proof.py
@@ -0,0 +1,92 @@
+##Propios
+import courseInfo2 as cursos
+import correo as enviar
+import tables as tabla
+##Sistema
+import pandas as pd
+
+
+def convert_allCourses():
+    c = cursos.get_all_courses()
+    c.to_excel("Cursos.xlsx")
+
+listaGrados = {"K3":126382145220,"1ro":126382145238,"2do":126382430010,
+               "3ro":126382430032,
+                "4to":126382430048,
+                "5to":126382430066,
+                "6to":126383120623,
+                "CM":283913077412,
+                }
+data = [
+        ['Nombre Del Alumno', 'Asistencia', 'Tareas', 'Proyecto' ]
+    ]
+
+def resumen_Actividades(mes,anio, Grado):
+    d = cursos.courses_list_activities(Grado, mes,anio)
+    users = cursos.get_users_by_idCourse(Grado)
+    Final = pd.DataFrame()
+    for i in d['CourseWorkID']:
+        actividades = cursos.sumbiss_student_courseWork(Grado,i)
+        Final = pd.concat([actividades, Final])
+        #Final = pd.merge(left=d, right=actividades, left_on='CourseWorkID', right_on='CourseWorkID')
+    Final2 = pd.merge(left=Final, right=d, left_on='CourseWorkID', right_on='CourseWorkID')
+    Final3 = pd.merge(left=Final2, right=users, left_on='StudentID', right_on='StudentID')
+    return Final3
+
+def generar_resumen(Clave):
+    users = cursos.get_users_by_idCourse(listaGrados[Clave])
+    c = resumen_Actividades(1,2021, listaGrados[Clave])
+    for i in range(len(users['StudentID'])):
+        try:
+            ##Enviar Explicacion
+            #users[EmailStudent]
+            enviar.enviar_correo_PDF(users['EmailStudent'][i],"Calificacion B3 (Archivo Explicacion)","Explicacion.xlsx")
+            ###Generacion de PDF
+            resumen = []
+            resumen2=[['Nombre Del Alumno', 'Asistencia', 'Tareas', 'Proyecto','Promedio Final' ]]
+            ###Generacion de Excel
+            print(users['StudentID'][i])
+            nuevo = c[c['StudentID'] == users['StudentID'][i]]
+            nuevo = nuevo.drop(['CourseID','CourseWorkID','StudentID','FechaCreacion_x'], axis=1)
+            Promedio = nuevo['Calificacion'].mean()
+            print("Promedio: "+str(Promedio))
+            ###Generacion de Excel
+            nuevo.to_excel("Resumen.xlsx")
+            ##Enviar Excel
+            enviar.enviar_correo_PDF(users['EmailStudent'][i],"Calificacion B3 (Archivo de Resumen Excel)","Resumen.xlsx")
+            ###Generacion de PDF
+            asistencia = int(input("Inserte Asistencia para "+ users['Student_Name'][i]+": "))
+            proyectos = int(input("Inserte proyectos para "+ users['Student_Name'][i]+": "))
+            resumen.append(users['Student_Name'][i]);resumen.append(str(asistencia))
+            resumen.append(str(Promedio));resumen.append(str(proyectos))
+            resumen.append(str((Promedio+proyectos+asistencia)/3))
+            resumen2.append(resumen)
+            tabla.generar_tabla(resumen2, "Tabla1")
+            enviar.enviar_correo_PDF(users['EmailStudent'][i],"Calificacion (Resumen PDF)","Tabla1.pdf")
+        except ValueError:
+            ##Enviar Explicacion
+            #users[EmailStudent]
+            enviar.enviar_correo_PDF(users['EmailStudent'][i],"Calificacion B3 (Archivo Explicacion)","Explicacion.xlsx")
+            ###Generacion de PDF
+            resumen = []
+            resumen2=[['Nombre Del Alumno', 'Asistencia', 'Tareas', 'Proyecto','Promedio Final' ]]
+            ###Generacion de Excel
+            print(users['StudentID'][i])
+            nuevo = c[c['StudentID'] == users['StudentID'][i]]
+            nuevo = nuevo.drop(['CourseID','CourseWorkID','StudentID','FechaCreacion_x'], axis=1)
+            Promedio = nuevo['Calificacion'].mean()
+            print("Promedio: "+str(Promedio))
+            ###Generacion de Excel
+            nuevo.to_excel("Resumen.xlsx")
+            ##Enviar Excel
+            enviar.enviar_correo_PDF(users['EmailStudent'][i],"Calificacion B3 (Archivo de Resumen Excel)","Resumen.xlsx")
+            ###Generacion de PDF
+            asistencia = int(input("Inserte Asistencia para "+ users['Student_Name'][i]+": "))
+            proyectos = int(input("Inserte proyectos para "+ users['Student_Name'][i]+": "))
+            resumen.append(users['Student_Name'][i]);resumen.append(str(asistencia))
+            resumen.append(str(Promedio));resumen.append(str(proyectos))
+            resumen.append(str((Promedio+proyectos+asistencia)/3))
+            resumen2.append(resumen)
+            tabla.generar_tabla(resumen2, "Tabla1")
+            enviar.enviar_correo_PDF(users['EmailStudent'][i],"Calificacion (Resumen PDF)","Tabla1.pdf")            
+generar_resumen('3ro')
